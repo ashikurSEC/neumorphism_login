458dca804adbfff9c9d9421734d2e82a327928a5
Ashikur Rahman <ashikurrahman7194@gmail.com>
2024-08-09 00:28:04 +0600

    js file adding

---
+++ js/Neumorphism.js
@@ -0,0 +1,12 @@
+let $facebook = document.querySelector('.facebook');
+let $twitter = document.querySelector('.twitter');
+
+$facebook.addEventListener('click', () =>{
+    window.location = 'https://www.facebook.com/';
+});
+
+$twitter.addEventListener('click', () =>{
+    window.location = 'https://twitter.com/home';
+});
+
+
